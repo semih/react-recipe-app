b8d80d68602a9fd327c2a3f18551ef104f4cd7d5
semih <semihkirdinli@gmail.com>
2020-05-02 02:17:01 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      Hello from our recipe app
+      <br />
+      Hello from our recipe app
+    </div>
+  );
+}
+
+export default App;
